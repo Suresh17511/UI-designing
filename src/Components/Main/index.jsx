a528d332d89d182f9874933e77bfc404bb44b71a
Suresh17511 <suresh.chidurala17511@gmail.com>
2021-12-15 10:39:41 +0530

    UI Design

---
+++ src/Components/Main/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "./HeroSection";
+import Testimonials from "./Testimonials";
+
+const Main = () => {
+  return (
+    <main>
+      <HeroSection />
+      <Testimonials />
+    </main>
+  );
+};
+
+export default Main;
